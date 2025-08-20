5e297f2b568da4f12753633ad5504c96b92e133e
HP <HP@LAPTOP-463TH5EG>
2025-08-20 09:33:14 +0530

    Project2

---
+++ CourseManagementModule/src/main/java/com/tns/course/exception/ServiceException.java
@@ -0,0 +1,13 @@
+package com.tns.course.exception;
+
+@SuppressWarnings("serial")
+public class ServiceException extends RuntimeException {
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
